28aa695aa500f6170041b59978c5288a8f4f6616
wonsky <pidborskii17@gmail.com>
2024-05-02 22:18:32 +0200

    implemented crud views for tags and notes

--- web_notes/views.py
+++ web_notes/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpRequest
+from django.urls import reverse_lazy, reverse
+from django.views import generic
 
-# Create your views here.
+from web_notes.forms import TagForm, NoteForm
+from web_notes.models import Note, Tag
+
+
+class NoteListView(generic.ListView):
+    model = Note
+    queryset = Note.objects.prefetch_related("tags")
+
+
+class NoteCreateView(generic.CreateView):
+    model = Note
+    form_class = NoteForm
+    success_url = reverse_lazy("notes:note-list")
+
+
+class NoteUpdateView(generic.UpdateView):
+    model = Note
+    success_url = reverse_lazy("notes:note-list")
+
+
+class NoteDeleteView(generic.DeleteView):
+    model = Note
+    success_url = reverse_lazy("notes:note-list")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("notes:tags-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    success_url = reverse_lazy("notes:tags-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("notes:tags-list")
