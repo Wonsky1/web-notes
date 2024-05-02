ff0428675014845c37912be6a7d5a38b2ddf6d34
wonsky <pidborskii17@gmail.com>
2024-05-02 22:20:24 +0200

    added django_select2 to installed apps

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "web_notes",
     "crispy_forms",
     "crispy_bootstrap4",
+    "django_select2",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
@@ -130,9 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
