39312ed978378b647c6fb8a22dabbaaeb75e1c9f
wonsky <pidborskii17@gmail.com>
2024-05-02 22:20:41 +0200

    added django_select2 to urls

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("web_notes.urls", namespace="web_notes"))
+    path("", include("web_notes.urls", namespace="web_notes")),
+    path('django_select2/', include('django_select2.urls')),
 ]
