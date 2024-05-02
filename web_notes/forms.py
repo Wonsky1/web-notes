69e836e6980838424b23b4cc2e73fa96b14f0426
wonsky <pidborskii17@gmail.com>
2024-05-02 22:19:22 +0200

    implemented forms for tags and notes

---
+++ web_notes/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django_select2.forms import ModelSelect2MultipleWidget
+
+from web_notes.models import Tag, Note
+
+
+class NoteForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=ModelSelect2MultipleWidget(
+            model=Tag,
+            search_fields=['name__icontains'],
+            attrs={'data-minimum-input-length': 0}
+        ),
+        required=False,
+    )
+    class Meta:
+        model = Note
+        fields = ["name", "description", "tags"]
+
+
+class TagForm(forms.ModelForm):
+    color = forms.CharField(
+        label='hex_color', max_length=7,
+        widget=forms.TextInput(attrs={'type': 'color'})
+    )
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
