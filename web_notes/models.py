6c3f8c84fe02cdd23d357e6f5e5dbfe1701580e1
wonsky <pidborskii17@gmail.com>
2024-05-02 12:46:43 +0200

    created models

--- web_notes/models.py
+++ web_notes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=63)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="notes")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("created_at", )
