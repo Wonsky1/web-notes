a8126d6889027d46090be5789eed212582f1ef46
wonsky <pidborskii17@gmail.com>
2024-05-02 14:11:42 +0200

    added is_pinned and is_favourite fields for Note model

--- web_notes/models.py
+++ web_notes/models.py
@@ -12,10 +12,12 @@ class Note(models.Model):
     name = models.CharField(max_length=63)
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name="notes")
+    tags = models.ManyToManyField(Tag, related_name="notes", blank=True)
+    is_pinned = models.BooleanField(default=False)
+    is_favourite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ("created_at", )
+        ordering = ("-is_pinned", "-created_at", )
