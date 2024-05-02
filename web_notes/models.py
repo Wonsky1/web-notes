d62b14f42e5f01b9846dc569029549f07c5e26dd
wonsky <pidborskii17@gmail.com>
2024-05-02 22:19:57 +0200

    implemented color field for tags is_archived field for notes

--- web_notes/models.py
+++ web_notes/models.py
@@ -1,11 +1,26 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def validate_color(value):
+    if not value.startswith('#') or len(value) != 7:
+        raise ValidationError('Invalid color format (must be #RRGGBB)')
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=63)
+    color = models.CharField(max_length=7, default="#FF0000", validators=[validate_color])
 
     def __str__(self):
         return self.name
+
+    def clean_color(self):
+        color = self.cleaned_data['color']
+        if not color.startswith('#') or len(color) != 7:
+            raise ValidationError('Invalid color format (must be #RRGGBB)')
+        return color
+
+
 
 
 class Note(models.Model):
@@ -15,6 +30,7 @@ class Note(models.Model):
     tags = models.ManyToManyField(Tag, related_name="notes", blank=True)
     is_pinned = models.BooleanField(default=False)
     is_favourite = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
