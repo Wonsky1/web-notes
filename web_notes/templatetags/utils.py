d2e4f5306592d9dd0b6e793f8c8327af3cecf8af
wonsky <pidborskii17@gmail.com>
2024-05-04 00:19:46 +0200

    implemented templatetag to show only x amount of symbols in description

---
+++ web_notes/templatetags/utils.py
@@ -0,0 +1,22 @@
+from django import template
+from textwrap import shorten
+
+
+register = template.Library()
+
+
+@register.filter(name='truncate_text')
+def truncate_text(text, limit=100, ending="..."):
+    """
+    Truncates text using the textwrap module.
+
+    Args:
+        text: The text to be truncated.
+        limit: The maximum number of symbols (default: 100).
+        ending: The string to append to the truncated text (default: "...").
+
+    Returns:
+        The truncated text.
+    """
+
+    return shorten(text, width=limit, placeholder=ending)
