72fb1ecd28d8a1b083d996884fd6ba372553bf8b
wonsky <pidborskii17@gmail.com>
2024-05-04 19:41:57 +0200

    implemented custom model for counting words in description

--- web_notes/models.py
+++ web_notes/models.py
@@ -21,6 +21,34 @@ class Tag(models.Model):
         return color
 
 
+class WordCountField(models.IntegerField):
+
+    def __init__(self, unique_count = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.unique_count = unique_count
+
+    def contribute_to_class(self, cls, name, **kwargs):
+        super().contribute_to_class(cls, name, **kwargs)
+        models.signals.post_save.connect(self.update_word_count, sender=cls)
+
+    def update_word_count(self, instance, **kwargs):
+        if self.unique_count:
+            word_count = self.calculate_unique_word_count(instance)
+        else:
+            word_count = self.calculate_word_count(instance)
+        if getattr(instance, self.attname) != word_count:
+            setattr(instance, self.attname, word_count)
+            instance.save(update_fields=[self.attname])
+
+    @staticmethod
+    def calculate_word_count(instance):
+        description = getattr(instance, 'description', '')
+        return len(description.split())
+
+    @staticmethod
+    def calculate_unique_word_count(instance):
+        description = getattr(instance, 'description', '')
+        return len(set(description.split()))
 
 
 class Note(models.Model):
@@ -31,6 +59,8 @@ class Note(models.Model):
     is_pinned = models.BooleanField(default=False)
     is_favourite = models.BooleanField(default=False)
     is_archived = models.BooleanField(default=False)
+    word_count = WordCountField(default=0, editable=False)
+    unique_word_count = WordCountField(default=0, editable=False, unique_count=True)
 
     def __str__(self):
         return self.name
