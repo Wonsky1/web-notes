c10911d857899a7843654349e2ae176f9f5fecfe
wonsky <pidborskii17@gmail.com>
2024-05-04 19:44:40 +0200

    blacked config folder, removed unnecessary comments

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("web_notes.urls", namespace="web_notes")),
-    path('django_select2/', include('django_select2.urls')),
+    path("django_select2/", include("django_select2.urls")),
 ]
