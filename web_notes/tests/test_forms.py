ef6989e764c7b2a930cbec51c7f36f96fb8c0e47
wonsky <pidborskii17@gmail.com>
2024-05-05 21:42:36 +0200

    uncommented tests

--- web_notes/tests/test_forms.py
+++ web_notes/tests/test_forms.py
@@ -1,45 +1,45 @@
-# from django.test import TestCase
-# from web_notes.forms import NoteForm, TagForm
-#
-#
-# class NoteFormTests(TestCase):
-#     def test_note_form_valid(self):
-#         form_data = {
-#             "name": "Test Note",
-#             "description": "This is a test note",
-#         }
-#         form = NoteForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#
-#     def test_note_form_invalid(self):
-#         form_data = {
-#             "description": "This is a test note",
-#         }
-#         form = NoteForm(data=form_data)
-#         self.assertFalse(form.is_valid())
-#
-#
-# class TagFormTests(TestCase):
-#     def test_tag_form_valid(self):
-#         form_data = {
-#             "name": "Test Tag",
-#             "color": "#FF0000",
-#         }
-#         form = TagForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#
-#     def test_tag_form_invalid(self):
-#         form_data = {
-#             "name": "Test Tag",
-#         }
-#         form = TagForm(data=form_data)
-#         self.assertFalse(form.is_valid())
-#
-#     def test_tag_color_validation(self):
-#         form_data = {
-#             "name": "Test Tag",
-#             "color": "invalid_color",
-#         }
-#         form = TagForm(data=form_data)
-#         self.assertFalse(form.is_valid())
-#         self.assertIn("color", form.errors)
+from django.test import TestCase
+from web_notes.forms import NoteForm, TagForm
+
+
+class NoteFormTests(TestCase):
+    def test_note_form_valid(self):
+        form_data = {
+            "name": "Test Note",
+            "description": "This is a test note",
+        }
+        form = NoteForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_note_form_invalid(self):
+        form_data = {
+            "description": "This is a test note",
+        }
+        form = NoteForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class TagFormTests(TestCase):
+    def test_tag_form_valid(self):
+        form_data = {
+            "name": "Test Tag",
+            "color": "#FF0000",
+        }
+        form = TagForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_tag_form_invalid(self):
+        form_data = {
+            "name": "Test Tag",
+        }
+        form = TagForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_tag_color_validation(self):
+        form_data = {
+            "name": "Test Tag",
+            "color": "invalid_color",
+        }
+        form = TagForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("color", form.errors)
--- web_notes/tests/test_models.py
+++ web_notes/tests/test_models.py
@@ -1,20 +1,20 @@
-# from django.test import TestCase
-# from web_notes.models import Tag, Note
-#
-#
-# class TagModelTestCase(TestCase):
-#     def test_tag_creation(self):
-#         tag = Tag.objects.create(name="Test Tag", color="#00FF00")
-#         self.assertEqual(tag.name, "Test Tag")
-#         self.assertEqual(tag.color, "#00FF00")
-#
-#
-# class NoteModelTestCase(TestCase):
-#     def test_note_creation(self):
-#         note = Note.objects.create(name="Test Note", description="one two one")
-#         self.assertEqual(note.name, "Test Note")
-#         self.assertEqual(note.is_archived, False)
-#         self.assertEqual(note.is_favourite, False)
-#         self.assertEqual(note.is_pinned, False)
-#         self.assertEqual(note.word_count, 3)
-#         self.assertEqual(note.unique_word_count, 2)
+from django.test import TestCase
+from web_notes.models import Tag, Note
+
+
+class TagModelTestCase(TestCase):
+    def test_tag_creation(self):
+        tag = Tag.objects.create(name="Test Tag", color="#00FF00")
+        self.assertEqual(tag.name, "Test Tag")
+        self.assertEqual(tag.color, "#00FF00")
+
+
+class NoteModelTestCase(TestCase):
+    def test_note_creation(self):
+        note = Note.objects.create(name="Test Note", description="one two one")
+        self.assertEqual(note.name, "Test Note")
+        self.assertEqual(note.is_archived, False)
+        self.assertEqual(note.is_favourite, False)
+        self.assertEqual(note.is_pinned, False)
+        self.assertEqual(note.word_count, 3)
+        self.assertEqual(note.unique_word_count, 2)
