7a9da9d8ba4d5729f593e30e70c6d648767c3bd1
wonsky <pidborskii17@gmail.com>
2024-05-05 15:26:57 +0200

    Added comments for those who wants to use sqlite instead of postgresql

--- config/settings.py
+++ config/settings.py
@@ -68,10 +68,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
-
-DATABASE_URL = "postgres://egnfweak:VUWTawVSkKtP_mkYBEBhV6hNbeW3O-2A@cornelius.db.elephantsql.com/egnfweak"
+db_from_env = dj_database_url.config(conn_max_age=500) # Comment this line if you want to use sqlite
+DATABASES["default"].update(db_from_env) # Comment this line if you want to use sqlite
 
 AUTH_PASSWORD_VALIDATORS = [
     {
